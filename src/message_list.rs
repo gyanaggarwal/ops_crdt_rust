6f9c2a8aac790eedcd9a3331578bdb21755e96bd
Gyanendra Aggarwal <gyanendra.aggarwal@gmail.com>
2024-08-27 16:04:23 -0500

    added concurrent_msg_list to message_list module

--- src/message_list.rs
+++ src/message_list.rs
@@ -4,8 +4,8 @@ use crate::vector_clock::{VectorClock, VectorClockError, VCOrdering};
 use crate::message_data::NodeUpdateMsg;
 use crate::{NodeType, LCType};
 
-pub fn remove_causally_stable<OpsValue: Clone+PartialEq>(cs_vc: &VectorClock, 
-    msg_list: &HashMap<(NodeType, LCType), NodeUpdateMsg<OpsValue>>) -> 
+pub fn remove_causally_stable<OpsValue: Clone+PartialEq>
+    (cs_vc: &VectorClock, msg_list: &HashMap<(NodeType, LCType), NodeUpdateMsg<OpsValue>>) -> 
     Result<HashMap<(NodeType, LCType), NodeUpdateMsg<OpsValue>>, VectorClockError> {
 
     let mut new_list = HashMap::new();
@@ -17,4 +17,20 @@ pub fn remove_causally_stable<OpsValue: Clone+PartialEq>(cs_vc: &VectorClock,
         }
     }
     Ok(new_list)    
+}
+
+pub fn concurrent_msg_list<OpsValue: Clone+PartialEq>
+    (msg_vc: &VectorClock, msg_list: &HashMap<(NodeType, LCType), NodeUpdateMsg<OpsValue>>, check_value: &Option<OpsValue>) ->
+    Result<Vec<NodeUpdateMsg<OpsValue>>, VectorClockError> {
+    let mut clist = Vec::new();
+
+    for msg in msg_list.values() {
+        let cmp_msg = msg_vc.cmp_vc(&msg.node_vector_clock)? == VCOrdering::VCCN;
+        let check_msg = msg.user_update_msg.ops_instance.check(check_value);
+        if cmp_msg && check_msg {
+            clist.push(msg.clone());
+        }
+    }
+
+    Ok(clist)
 }
