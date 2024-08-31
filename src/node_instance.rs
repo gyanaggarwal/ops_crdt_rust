274272ea18fa5a41bb82931d9c0c14d0f4e2e19f
Gyanendra Aggarwal <gyanendra.aggarwal@gmail.com>
2024-08-31 06:15:05 -0500

    added methods to node_instance

--- src/node_instance.rs
+++ src/node_instance.rs
@@ -20,23 +20,23 @@ use crate::vector_clock::VectorClockError;
 
 #[derive(Debug)]
 pub struct NodeInstance {
-    pub node: NodeType,
+    pub node:          NodeType,
     pub add_mult_crdt: CRDT<IntMultCrdtValue, IntMultOpsValue, AddMult>,
-    pub ewflag_crdt: CRDT<EDFlagCrdtValue, EDFlagOpsValue, EWFlag>,
-    pub dwflag_crdt: CRDT<EDFlagCrdtValue, EDFlagOpsValue, DWFlag>,
-    pub awset_crdt: CRDT<HashSet<ARSetOpsValue>, ARSetOpsValue, AWSet>,
-    pub rwset_crdt: CRDT<HashSet<ARSetOpsValue>, ARSetOpsValue, RWSet>,
-    pub pncnt_crdt: CRDT<PNCounterData, PNCntOpsValue, PNCounter>
+    pub ewflag_crdt:   CRDT<EDFlagCrdtValue, EDFlagOpsValue, EWFlag>,
+    pub dwflag_crdt:   CRDT<EDFlagCrdtValue, EDFlagOpsValue, DWFlag>,
+    pub awset_crdt:    CRDT<HashSet<ARSetOpsValue>, ARSetOpsValue, AWSet>,
+    pub rwset_crdt:    CRDT<HashSet<ARSetOpsValue>, ARSetOpsValue, RWSet>,
+    pub pncnt_crdt:    CRDT<PNCounterData, PNCntOpsValue, PNCounter>
 }
 
 impl NodeInstance {
     pub fn new(node: NodeType) -> Result<Self, VectorClockError> {
         let add_mult_crdt: CRDT<IntMultCrdtValue, IntMultOpsValue, AddMult> = CRDT::new(node, 0)?;
-        let ewflag_crdt: CRDT<EDFlagCrdtValue, EDFlagOpsValue, EWFlag> = CRDT::new(node, EDFlag::Enabled)?;
-        let dwflag_crdt: CRDT<EDFlagCrdtValue, EDFlagOpsValue, DWFlag> = CRDT::new(node, EDFlag::Disabled)?;
-        let awset_crdt: CRDT<HashSet<ARSetOpsValue>, ARSetOpsValue, AWSet> = CRDT::new(node, HashSet::<ARSetOpsValue>::new())?;
-        let rwset_crdt: CRDT<HashSet<ARSetOpsValue>, ARSetOpsValue, RWSet> = CRDT::new(node, HashSet::<ARSetOpsValue>::new())?;
-        let pncnt_crdt: CRDT<PNCounterData, PNCntOpsValue, PNCounter> = CRDT::new(node, PNCounterData::new())?;
+        let ewflag_crdt:   CRDT<EDFlagCrdtValue, EDFlagOpsValue, EWFlag> = CRDT::new(node, EDFlag::Enabled)?;
+        let dwflag_crdt:   CRDT<EDFlagCrdtValue, EDFlagOpsValue, DWFlag> = CRDT::new(node, EDFlag::Disabled)?;
+        let awset_crdt:    CRDT<HashSet<ARSetOpsValue>, ARSetOpsValue, AWSet> = CRDT::new(node, HashSet::<ARSetOpsValue>::new())?;
+        let rwset_crdt:    CRDT<HashSet<ARSetOpsValue>, ARSetOpsValue, RWSet> = CRDT::new(node, HashSet::<ARSetOpsValue>::new())?;
+        let pncnt_crdt:    CRDT<PNCounterData, PNCntOpsValue, PNCounter> = CRDT::new(node, PNCounterData::new())?;
         Ok(Self{node, add_mult_crdt, ewflag_crdt, dwflag_crdt, awset_crdt, rwset_crdt, pncnt_crdt})
     }
 
@@ -57,5 +57,45 @@ impl NodeInstance {
 
     pub fn get_mult_ops_add_mult(&self, value: IntMultOpsValue) -> OpsInstance<IntMultOpsValue> {
         self.add_mult_crdt.get_add_ops(value)
+    }
+
+    pub fn get_add_ops_ewflag(&self) -> OpsInstance<EDFlagOpsValue> {
+        self.ewflag_crdt.get_add_ops()
+    }
+
+    pub fn get_mult_ops_ewflag(&self) -> OpsInstance<EDFlagOpsValue> {
+        self.ewflag_crdt.get_add_ops()
+    }
+
+    pub fn get_add_ops_dwflag(&self) -> OpsInstance<EDFlagOpsValue> {
+        self.dwflag_crdt.get_add_ops()
+    }
+
+    pub fn get_mult_ops_dwflag(&self) -> OpsInstance<EDFlagOpsValue> {
+        self.dwflag_crdt.get_add_ops()
+    }
+
+    pub fn get_add_ops_awset(&self, value: ARSetOpsValue) -> OpsInstance<ARSetOpsValue> {
+        self.awset_crdt.get_add_ops(value)
+    }
+
+    pub fn get_mult_ops_awset(&self, value: ARSetOpsValue) -> OpsInstance<ARSetOpsValue> {
+        self.awset_crdt.get_add_ops(value)
+    }
+
+    pub fn get_add_ops_rwset(&self, value: ARSetOpsValue) -> OpsInstance<ARSetOpsValue> {
+        self.rwset_crdt.get_add_ops(value)
+    }
+
+    pub fn get_mult_ops_rwset(&self, value: ARSetOpsValue) -> OpsInstance<ARSetOpsValue> {
+        self.rwset_crdt.get_add_ops(value)
+    }
+
+    pub fn get_add_ops_pncnt(&self, value: PNCntOpsValue) -> OpsInstance<PNCntOpsValue> {
+        self.pncnt_crdt.get_add_ops(value)
+    }
+
+    pub fn get_mult_ops_pncnt(&self, value: PNCntOpsValue) -> OpsInstance<PNCntOpsValue> {
+        self.pncnt_crdt.get_add_ops(value)
     }
 }
